f24386f9f2aca93f491122ff289442a16390c82a
Thais Martins <contato@thaismartins.rocks>
2015-10-04 14:00:24 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html lang="pt" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="pt" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="pt" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="pt" class="no-js"> <!--<![endif]-->
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <title>GypaNews</title>
+
+    <link href='https://fonts.googleapis.com/css?family=Raleway:100,400,300,600,800' rel='stylesheet' type='text/css'>
+
+    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel='stylesheet' type='text/css'>
+    <link href="bower_components/fontawesome/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link href="bower_components/lightbox2/dist/css/lightbox.css" rel='stylesheet' type='text/css'>
+
+    <link href="style.css" rel='stylesheet' type='text/css'>
+    <link href="css/blog.css" rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<!--[if lt IE 7]>
+<p>Você está usando um navegador <strong>desatualizado</strong>. Por favor, <a href="http://browsehappy.com/">atualize-o</a> para ter uma melhor experiência.</p>
+<![endif]-->
+
+    <div class="container-fluid">
+       <header>
+           <div class="container">
+               <div class="logo col-xs-12 col-sm-2">
+                   <a href="#">
+                       <img src="images/logo.png" alt="GypaNews" title="GypaNews">
+                   </a>
+               </div>
+               <div class="ad col-sm-10 hidden-xs">
+                   <a href="#">
+                       <img src="http://placehold.it/728x90" alt="Anuncia Aqui" title="Anuncie Aqui">
+                   </a>
+               </div>
+           </div>
+           <nav class="col-xs-12">
+               <div class="container">
+                   <ul>
+                       <li><a href="#">Página Inicial</a></li>
+                       <li><a href="#">Quem Somos</a></li>
+                       <li><a href="#">Perfil</a></li>
+                       <li><a href="#">Social</a></li>
+                       <li><a href="#">Dicas</a></li>
+                       <li><a href="#">Entrevistas</a></li>
+                       <li><a href="#">Reportagens</a></li>
+                       <li><a href="#">Blog</a></li>
+                       <li><a href="#">Guia</a></li>
+                   </ul>
+               </div>
+           </nav>
+       </header>
+    </div>
+
+    <script src="js/main.js"></script>
+</body>
+</html>
